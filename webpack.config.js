bd9f0432df1f1cffcc58be509e866e050ecf3850
Sky <2499126676@qq.com>
2023-01-25 22:06:06 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "simpleMVVM",
+    libraryTarget: "umd",
+    libraryExport: "default",
+  },
+  optimization: {
+    minimize: false,
+  },
+};
