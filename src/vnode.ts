1bfcfd76f3328d3a008143c255de0ed6bbd90352
Sky <2499126676@qq.com>
2023-01-27 01:30:40 +0800

    添加类型约束

--- src/vnode.ts
+++ src/vnode.ts
@@ -1,13 +1,44 @@
 import { elemOption } from "./compiler";
-class VNode {
+
+export enum NodeType {
+  Element = 1,
+  Attr,
+  Text,
+}
+
+// 虚拟节点参数约束
+type VNodeOptions = {
+  tagName?: string;
+  attrs?: elemOption;
+  event?: EventOptions;
+  children?: VNode[];
+  el: Element | Text | undefined; // 存放真实元素对象
+  nodeValue?: string; // 文本元素的值
+  type: NodeType;
+};
+
+// 事件参数约束
+type EventOptions = {
+  [props: string]: EventListener | EventListenerObject;
+};
+
+export class VNode {
   tagName?: string;
   attrs: elemOption = {};
-  event = {};
+  event: EventOptions = {};
   children: VNode[] = [];
   el: Element | Text | undefined;
   nodeValue?: string;
-  type;
-  constructor({ tagName, attrs, event, children, nodeValue, type }) {
+  type: NodeType;
+
+  constructor({
+    tagName,
+    attrs,
+    event,
+    children,
+    nodeValue,
+    type,
+  }: VNodeOptions) {
     this.tagName = tagName;
     attrs && (this.attrs = attrs);
     event && (this.event = event);
