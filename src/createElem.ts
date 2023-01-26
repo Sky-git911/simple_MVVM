e521f59c2c9b707b05ed3a7ba1720ff80519f677
Sky <2499126676@qq.com>
2023-01-27 01:31:37 +0800

    创建元素或创建文本

---
+++ src/createElem.ts
@@ -0,0 +1,45 @@
+import { elemOption } from "./compiler";
+import { VNode, NodeType } from "./vnode";
+
+type createElement = (
+  tagName: string,
+  options: elemOption,
+  children: Array<VNode | string>
+) => VNode;
+
+type createText = (value: string) => VNode;
+
+/**
+ * 创建元素
+ */
+const createElement: createElement = function (
+  tagName,
+  { attrs, event },
+  children = []
+) {
+  let vnodeList: VNode[] = children.map((item) => {
+    if (typeof item === "string") {
+      return createText(item);
+    } else {
+      return item;
+    }
+  });
+
+  return new VNode({
+    tagName,
+    attrs,
+    event,
+    children: vnodeList,
+    type: NodeType.Element,
+  });
+};
+
+/**
+ * 创建文本
+ */
+const createText: createText = function (value) {
+  return new VNode({
+    nodeValue: value,
+    type: NodeType.Text,
+  });
+};
