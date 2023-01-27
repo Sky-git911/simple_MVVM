2b7fef14c569742e4fc10b7692ab77ef902ac17e
Sky <2499126676@qq.com>
2023-01-27 13:58:57 +0800

    完善初始化元素节点时的 patch 方法

--- src/patch.ts
+++ src/patch.ts
@@ -1,8 +1,48 @@
 import { ComponentInstance } from "./createAPP";
-import { VNode } from "./vnode";
+import { getValue } from "./utils";
+import { NodeType, VNode } from "./vnode";
 
 export function patch(
   oldVNode: VNode | undefined,
   newVNode: VNode,
   instance: ComponentInstance
-) {}
+) {
+  // 初始化时的挂载
+  if (!oldVNode) {
+    let el = vnodeToElem(newVNode);
+    if (el && instance.el) {
+      instance.el.parentNode?.replaceChild(el, instance.el);
+    }
+    return;
+  }
+}
+
+function vnodeToElem(vnode: VNode) {
+  if (vnode.type === NodeType.Text) {
+    let el = document.createTextNode(getValue(vnode.nodeValue) || "");
+    vnode.el = el;
+    return el;
+  }
+  //   此时已经是 文本 节点
+  if (!vnode.tagName) return;
+
+  let el = document.createElement(vnode.tagName);
+  //   设置元素属性
+  for (let key in vnode.attrs) {
+    el.setAttribute(key, vnode.attrs[key]);
+  }
+  //   绑定元素事件
+  for (let key in vnode.event) {
+    el.addEventListener(key, vnode.event[key]);
+  }
+  //   循环递归生成子元素
+  if (vnode.children.length > 0) {
+    vnode.children.forEach((v) => {
+      let child = vnodeToElem(v);
+      child && el.appendChild(child);
+    });
+  }
+
+  vnode.el = el;
+  return el;
+}
