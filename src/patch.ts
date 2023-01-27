f46bdd44b797b6c3536745b595de52ce4cb02f89
Sky <2499126676@qq.com>
2023-01-27 13:38:08 +0800

    patch 函数

---
+++ src/patch.ts
@@ -0,0 +1,8 @@
+import { ComponentInstance } from "./createAPP";
+import { VNode } from "./vnode";
+
+export function patch(
+  oldVNode: VNode | undefined,
+  newVNode: VNode,
+  instance: ComponentInstance
+) {}
