83b0e4991f992ca5039ba5c3fabec4676179e021
Sky <2499126676@qq.com>
2023-01-27 15:38:10 +0800

    增加diff判断是否是相同节点

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,22 @@
+import { VNode } from "./vnode/vnode";
+
+/**
+ * 判断是否为 object 对象
+ * @param target unknown
+ * @returns boolean
+ */
 export function isObject(target: unknown): boolean {
   return typeof target === "object" && target != null;
 }
 
-export const isRef = function (target: any) {
+/**
+ * 判断是否是 ref 基本属性值
+ * @param target any
+ * @returns boolean
+ */
+export function isRef(target: any): boolean {
   return isObject(target) && "isRef" in target && target.isRef;
-};
+}
 
 interface ArrayLike {
   [index: number]: any;
@@ -15,6 +27,32 @@ export function toArray(arr: ArrayLike) {
   return Array.prototype.slice.call(arr);
 }
 
+/**
+ * 获取 target 的值
+ * @param target any
+ * @returns any
+ */
 export function getValue(target: any) {
   return isRef(target) ? target.value : target;
+}
+
+/**
+ * diff判断是否是相同节点
+ * @param oldVNode VNode
+ * @param newVNode VNode
+ * @returns boolean
+ */
+export function isSameVNode(oldVNode: VNode, newVNode: VNode): boolean {
+  if (
+    oldVNode.type === newVNode.type &&
+    oldVNode.tagName === "input" &&
+    newVNode.tagName === "input"
+  ) {
+    if (!oldVNode.attrs.type && !newVNode.attrs.type) return true;
+    return oldVNode.attrs.type === newVNode.attrs.type;
+  } else {
+    return (
+      oldVNode.type === newVNode.type && oldVNode.tagName === newVNode.tagName
+    );
+  }
 }
