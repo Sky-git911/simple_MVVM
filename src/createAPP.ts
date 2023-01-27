b6b88ba35dd02518bfce83b84bb77e0d9f36fa3e
Sky <2499126676@qq.com>
2023-01-27 12:05:19 +0800

    补充mount方法，补充类型约束

--- src/createAPP.ts
+++ src/createAPP.ts
@@ -1,17 +1,54 @@
+import { compile } from "./compiler";
 import { createElement } from "./createElem";
 import { isRef } from "./utils";
+
+interface setupOptions {
+  setup?: (fn: createElement) => any;
+}
+
+interface App {
+  $option: setupOptions;
+  component: ComponentInstance;
+  mount: (selector: string) => void;
+}
+
+interface ComponentInstance {
+  $option: setupOptions;
+  render?: Function;
+  el?: Element;
+  _c: createElement;
+  _v: (target: any) => any;
+  proxy: object;
+}
 
 /**
  * 构建实例
  * mount 方法：挂载页面
  * 大概流程：compiler 处理模版得到 render 函数，  处理 setup 得到数据
  */
-const createApp = function (options) {
+const createApp = function (options: setupOptions) {
   let instance = createInstance(options);
-  let app = {
+  /**
+   * app 实例中
+   * $option 储存原始配置
+   * component 存组件实例
+   * mount 用于向目标元素挂载
+   * 其中会有 _c、_v 供 render 函数调用
+   */
+  let app: App = {
     $option: options,
     component: instance,
-    mount(selector) {},
+    mount(selector) {
+      let el = document.querySelector(selector);
+      if (!el) return;
+
+      let instance = this.component;
+      instance.el = el;
+      //拿到需要挂载的 dom 元素后，将目标 dom 树编译成 render 函数
+      instance.render = compile(el);
+
+      processSetup(instance);
+    },
   };
   return app;
 };
@@ -20,7 +57,7 @@ const getValue = function (target: any) {
   return isRef(target) ? target.value : target;
 };
 
-const createInstance = function (options) {
+const createInstance = function (options: setupOptions): ComponentInstance {
   let instance = {
     $option: options,
     _c: createElement,
@@ -29,3 +66,8 @@ const createInstance = function (options) {
   };
   return instance;
 };
+
+/** 
+ 处理实例中的 setup 函数
+ */
+const processSetup = function (instance: ComponentInstance) {};
