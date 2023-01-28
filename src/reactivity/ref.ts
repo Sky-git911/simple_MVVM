5f0b710c7f365811a4986a25d66ab8d87f4c86d1
Sky <2499126676@qq.com>
2023-01-28 16:21:16 +0800

    优化结构

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,6 +1,11 @@
 import { isObject, isRef } from "../utils";
 import { track, trigger } from "./reactive";
 
+/**
+ * 基本数据类型响应式
+ */
+
+// 方法一
 export const ref = function (value: any) {
   if (isObject(value)) {
     if (isRef(value)) return value;
@@ -22,3 +27,32 @@ export const ref = function (value: any) {
   });
   return result;
 };
+
+// 方法二
+
+// export class RefImpl<T = any> {
+//   private _value: T;
+//   private readonly _target: Record<any, any>;
+//   private isRef: Record<any, boolean>;
+
+//   constructor(value: T) {
+//     this._value = value;
+//     this._target = { value: this._value };
+//     this.isRef = { value: true };
+//   }
+
+//   get value() {
+//     track(this._target, "value");
+//     return this._value;
+//   }
+
+//   set value(value) {
+//     this._value = value;
+//     this._target.value = this._value;
+//     trigger(this._target, "value");
+//   }
+// }
+
+// export function ref<T extends any>(value: T): RefImpl<T> {
+//   return new RefImpl(value);
+// }
